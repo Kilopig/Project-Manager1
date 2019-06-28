23a460b639c98eddedb71f0cba3758badf9ed96a
EC2 Default User <ec2-user@ip-172-31-20-239.us-east-2.compute.internal>
2019-06-28 17:55:16 +0000

    smell

--- app/models/API.py
+++ app/models/API.py
@@ -13,13 +13,13 @@ def showPrice(name):
     if name == "microwave":
         results = json.load(urllib.urlopen("http://www.pricetree.com/dev/api.ashx?pricetreeId=124120&apikey=7770AD31-382F-4D32-8C36-3743C0271699"))
     if name == "laptop":
-        results = json.load(urllib.urlopen("http://www.pricetree.com/dev/api.ashx?pricetreeId=123051&apikey=7770AD31-382F-4D32-8C36-3743C0271699"))
+        results = json.load(urllib.urlopen("http://www.pricetree.com/dev/api.ashx?pricetreeId=123229&apikey=7770AD31-382F-4D32-8C36-3743C0271699"))
     if name == "tv":
-        results = json.load(urllib.urlopen("http://www.pricetree.com/dev/api.ashx?pricetreeId=119326&apikey=7770AD31-382F-4D32-8C36-3743C0271699"))
+        results = json.load(urllib.urlopen("http://www.pricetree.com/dev/api.ashx?pricetreeId=119307&apikey=7770AD31-382F-4D32-8C36-3743C0271699"))
     if name == "printer":
         results = json.load(urllib.urlopen("http://www.pricetree.com/dev/api.ashx?pricetreeId=124333&apikey=7770AD31-382F-4D32-8C36-3743C0271699"))
     if name == "camera":
         results = json.load(urllib.urlopen("http://www.pricetree.com/dev/api.ashx?pricetreeId=120752&apikey=7770AD31-382F-4D32-8C36-3743C0271699"))
     if name == "wash":
-        results = json.load(urllib.urlopen("http://www.pricetree.com/dev/api.ashx?pricetreeId=122678&apikey=7770AD31-382F-4D32-8C36-3743C0271699"))
+        results = json.load(urllib.urlopen("http://www.pricetree.com/dev/api.ashx?pricetreeId=122743&apikey=7770AD31-382F-4D32-8C36-3743C0271699"))
     return results["data"];
