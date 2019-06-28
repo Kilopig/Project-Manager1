9bbfe61dbef172d0ad0627dc26cde5add001f975
EC2 Default User <ec2-user@ip-172-31-20-239.us-east-2.compute.internal>
2019-06-28 17:31:07 +0000

    hi

--- app/models/API.py
+++ app/models/API.py
@@ -12,4 +12,14 @@ def showPrice(name):
         results = json.load(urllib.urlopen("http://www.pricetree.com/dev/api.ashx?pricetreeId=122177&apikey=7770AD31-382F-4D32-8C36-3743C0271699"))
     if name == "microwave":
         results = json.load(urllib.urlopen("http://www.pricetree.com/dev/api.ashx?pricetreeId=124120&apikey=7770AD31-382F-4D32-8C36-3743C0271699"))
+    if name == "iphone":
+        results = json.load(urllib.urlopen("http://www.pricetree.com/dev/api.ashx?pricetreeId=82044&apikey=7770AD31-382F-4D32-8C36-3743C0271699"))
+    if name == "ipad":
+        results = json.load(urllib.urlopen("http://www.pricetree.com/dev/api.ashx?pricetreeId=757&apikey=7770AD31-382F-4D32-8C36-3743C0271699"))
+    if name == "fridge":
+        results = json.load(urllib.urlopen("http://www.pricetree.com/dev/api.ashx?pricetreeId=122177&apikey=7770AD31-382F-4D32-8C36-3743C0271699"))
+    if name == "microwave":
+        results = json.load(urllib.urlopen("http://www.pricetree.com/dev/api.ashx?pricetreeId=124120&apikey=7770AD31-382F-4D32-8C36-3743C0271699"))
+    if name == "iphone":
+        results = json.load(urllib.urlopen("http://www.pricetree.com/dev/api.ashx?pricetreeId=82044&apikey=7770AD31-382F-4D32-8C36-3743C0271699"))
     return results["data"];
--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,7 @@ from app.models import model, formopener, API
 @app.route('/index')
 def index():
     return render_template("index.html")
-    
+
 @app.route('/results', methods=["GET", "POST"])
 def results():
     userdata = dict(request.form)
@@ -25,13 +25,13 @@ def results():
     end_of_year = str(float(sales) * float(profit_per_product))
     end_of_year_sales = str(float(money) - float(end_of_year))
     goal = int((float(money) - float(end_of_year)) / float(profit_per_product))
-    # return ("It will cost " + buy_stock + " dollars to purchase " + str(buyinvt) + " of the " + product + ". If you sell all of your current inventory, you will make " + profit_cur + " dollars. If you sell your current inventory and the inventory you are purchasing, you will make " + profit_after + " dollars. So far you have made " + end_of_year + " dollars this year. To reach your goal, you need to make " + end_of_year_sales + " dollars or sell " + str(goal) + " of the " + product) 
+    # return ("It will cost " + buy_stock + " dollars to purchase " + str(buyinvt) + " of the " + product + ". If you sell all of your current inventory, you will make " + profit_cur + " dollars. If you sell your current inventory and the inventory you are purchasing, you will make " + profit_after + " dollars. So far you have made " + end_of_year + " dollars this year. To reach your goal, you need to make " + end_of_year_sales + " dollars or sell " + str(goal) + " of the " + product)
     return render_template("results.html", product = product, buy_stock = buy_stock, buyinvt = buyinvt, profit_per_product = profit_per_product, profit_cur = profit_cur, profit_after = profit_after, end_of_year = end_of_year, end_of_year_sales = end_of_year_sales, goal = goal)
 @app.route("/s")
 def s():
     return render_template("s.html")
 @app.route("/info", methods=["GET", "POST"])
-def info(): 
+def info():
     dat = dict(request.form)
     name = dat["1"][0]
     response = API.showPrice(name)
@@ -40,4 +40,3 @@ def info():
     price_US = float(response[0]['Best_Price'].replace(",", ""))/69.01
     response[0]['Seller_Name']
     return render_template('patrick_is_cool.html', data=response, price_US = price_US)
-    
