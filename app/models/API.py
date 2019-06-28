d4ba8c6621d9bd4325b1464e796d19e55970b79c
EC2 Default User <ec2-user@ip-172-31-20-239.us-east-2.compute.internal>
2019-06-28 17:41:09 +0000

    your mom

--- app/models/API.py
+++ app/models/API.py
@@ -12,14 +12,14 @@ def showPrice(name):
         results = json.load(urllib.urlopen("http://www.pricetree.com/dev/api.ashx?pricetreeId=122177&apikey=7770AD31-382F-4D32-8C36-3743C0271699"))
     if name == "microwave":
         results = json.load(urllib.urlopen("http://www.pricetree.com/dev/api.ashx?pricetreeId=124120&apikey=7770AD31-382F-4D32-8C36-3743C0271699"))
-    if name == "iphone":
-        results = json.load(urllib.urlopen("http://www.pricetree.com/dev/api.ashx?pricetreeId=82044&apikey=7770AD31-382F-4D32-8C36-3743C0271699"))
-    if name == "ipad":
-        results = json.load(urllib.urlopen("http://www.pricetree.com/dev/api.ashx?pricetreeId=757&apikey=7770AD31-382F-4D32-8C36-3743C0271699"))
-    if name == "fridge":
-        results = json.load(urllib.urlopen("http://www.pricetree.com/dev/api.ashx?pricetreeId=122177&apikey=7770AD31-382F-4D32-8C36-3743C0271699"))
-    if name == "microwave":
-        results = json.load(urllib.urlopen("http://www.pricetree.com/dev/api.ashx?pricetreeId=124120&apikey=7770AD31-382F-4D32-8C36-3743C0271699"))
-    if name == "iphone":
-        results = json.load(urllib.urlopen("http://www.pricetree.com/dev/api.ashx?pricetreeId=82044&apikey=7770AD31-382F-4D32-8C36-3743C0271699"))
+    if name == "laptop":
+        results = json.load(urllib.urlopen("http://www.pricetree.com/dev/api.ashx?pricetreeId=123051&apikey=7770AD31-382F-4D32-8C36-3743C0271699"))
+    if name == "tv":
+        results = json.load(urllib.urlopen("http://www.pricetree.com/dev/api.ashx?pricetreeId=119326&apikey=7770AD31-382F-4D32-8C36-3743C0271699"))
+    if name == "printer":
+        results = json.load(urllib.urlopen("http://www.pricetree.com/dev/api.ashx?pricetreeId=124333&apikey=7770AD31-382F-4D32-8C36-3743C0271699"))
+    if name == "camera":
+        results = json.load(urllib.urlopen("http://www.pricetree.com/dev/api.ashx?pricetreeId=120752&apikey=7770AD31-382F-4D32-8C36-3743C0271699"))
+    if name == "wash":
+        results = json.load(urllib.urlopen("http://www.pricetree.com/dev/api.ashx?pricetreeId=122678&apikey=7770AD31-382F-4D32-8C36-3743C0271699"))
     return results["data"];
